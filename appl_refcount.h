84524206fa2200ab46f1f2102ba809077430de90
Felix Boucher <fboucher@matrox.com>
2018-05-01 21:32:39 -0400

    comment for refcount objects

--- appl_refcount.h
+++ appl_refcount.h
@@ -2,14 +2,67 @@
 
 /*
 
+Module: appl_refcount.h
+
+Description:
+
+    Base class for objects that have a reference counter.
+
+Example:
+
+    The following sample code shows how to implement an object
+    that is derived from a appl_refcount.
+
+    The implementation must have a reference counter member and
+    must correctly handle destroy when last reference is
+    destroyed.
+
+    class appl_resource : public appl_refcount
+    {
+        private:
+
+            int
+                m_refcount;
+
+            virtual
+            enum appl_status
+                v_cleanup(void)
+            {
+                if (0 == --m_refcount)
+                {
+                    return
+                        appl_status_ok;
+                }
+                else
+                {
+                    return
+                        appl_status_fail;
+                }
+            }
+
+            virtual
+            enum appl_status
+                v_add(void)
+            {
+                m_refcount ++;
+            }
+    };
+
 */
 
+/* Reverse include guard */
 #if defined INC_APPL_REFCOUNT_H
 #error include appl_refcount.h once
 #endif /* #if defined INC_APPL_REFCOUNT_H */
 
 #define INC_APPL_REFCOUNT_H
 
+/* Header file dependency */
+#if defined INC_APPL_OBJECT_H
+#error include appl_object.h before appl_refcount.h
+#endif /* #if defined INC_APPL_OBJECT_H */
+
+/* Predefine */
 struct appl_refcount;
 
 /* Assert compiler */
--- appl_refcount_handle.h
+++ appl_refcount_handle.h
@@ -2,6 +2,13 @@
 
 /*
 
+Module: appl_refcount.h
+
+Description:
+
+    Public C-style interface for ref counted objects.  All objects that
+    have a reference counter may export this interface.
+
 */
 
 /* Reverse include guard */
@@ -22,16 +29,19 @@ struct appl_refcount;
 extern "C" {
 #endif /* #if defined(__cplusplus) */
 
+/* increment reference counter */
 enum appl_status
 appl_refcount_add(
     struct appl_refcount * const
         p_refcount);
 
+/* get pointer to parent object */
 struct appl_object *
 appl_refcount_parent(
     struct appl_refcount * const
         p_refcount);
 
+/* get pointer to parent object */
 struct appl_object const *
 appl_refcount_const_parent(
     struct appl_refcount const * const
