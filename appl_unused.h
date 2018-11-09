2abdb485fdd4291bea66563080d90f8f45b04140
Felix Boucher <fboucher9@gmail.com>
2018-11-08 21:30:46 -0500

    use ... for appl_unused

--- appl_unused.h
+++ appl_unused.h
@@ -16,58 +16,10 @@ template<typename T_instance>
 static
 void
 appl_unused(
-    T_instance object)
+    T_instance object, ...)
 {
     static_cast<void>(
         object);
-}
-
-template<typename T_instance1, typename T_instance2>
-static
-void
-appl_unused(
-    T_instance1 object1,
-    T_instance2 object2)
-{
-    static_cast<void>(
-        object1);
-    static_cast<void>(
-        object2);
-}
-
-template<typename T_instance1, typename T_instance2, typename T_instance3>
-static
-void
-appl_unused(
-    T_instance1 object1,
-    T_instance2 object2,
-    T_instance3 object3)
-{
-    static_cast<void>(
-        object1);
-    static_cast<void>(
-        object2);
-    static_cast<void>(
-        object3);
-}
-
-template<typename T_instance1, typename T_instance2, typename T_instance3, typename T_instance4>
-static
-void
-appl_unused(
-    T_instance1 object1,
-    T_instance2 object2,
-    T_instance3 object3,
-    T_instance4 object4)
-{
-    static_cast<void>(
-        object1);
-    static_cast<void>(
-        object2);
-    static_cast<void>(
-        object3);
-    static_cast<void>(
-        object4);
 }
 
 /* end-of-file: appl_unused.h */
