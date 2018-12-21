9390e80800de65d5648c1fa60e66090eccb46362
Felix Boucher <fboucher9@gmail.com>
2018-12-21 08:31:05 -0500

    coverage for dictionary module

--- dict/appl_dict_test.cpp
+++ dict/appl_dict_test.cpp
@@ -48,7 +48,7 @@ void
                 p_dict);
 
             {
-                static unsigned char const g_name[] =
+                static unsigned char const g_name1[] =
                 {
                     'n',
                     'a',
@@ -56,24 +56,57 @@ void
                     'e'
                 };
 
-                static unsigned char g_value[1u] =
+                static unsigned char const g_name2[] =
+                {
+                    'w',
+                    'h',
+                    'a',
+                    't'
+                };
+
+                static unsigned char g_value1[1u] =
                 {
                     123u
+                };
+
+                static unsigned char g_value2[1u] =
+                {
+                    77u
                 };
 
                 void *
                     p_value;
 
+                appl_dict_get(
+                    p_dict,
+                    g_name1,
+                    g_name1 + sizeof(g_name1),
+                    &(
+                        p_value));
+
                 appl_dict_set(
                     p_dict,
-                    g_name,
-                    g_name + sizeof(g_name),
-                    g_value);
+                    g_name1,
+                    g_name1 + sizeof(g_name1),
+                    g_value1);
 
                 appl_dict_get(
                     p_dict,
-                    g_name,
-                    g_name + sizeof(g_name),
+                    g_name1,
+                    g_name1 + sizeof(g_name1),
+                    &(
+                        p_value));
+
+                appl_dict_set(
+                    p_dict,
+                    g_name1,
+                    g_name1 + sizeof(g_name1),
+                    g_value2);
+
+                appl_dict_get(
+                    p_dict,
+                    g_name2,
+                    g_name2 + sizeof(g_name2),
                     &(
                         p_value));
             }
